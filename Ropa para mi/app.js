ec62ab2e25112d6c5c0ab770744df25d922a92d1
rodrigo-reynoso <rodrigosreynoso33@gmail.com>
2020-06-22 20:43:07 -0300

    di estilo a la fecha

--- Ropa para mi/app.js
+++ Ropa para mi/app.js
@@ -3,18 +3,30 @@
  var fecha = new Date(); // objeto fecha actual // var porque es la unica que es modificable dentro de una function
 
 
-function textoFecha(fecha){
+function textodia(fecha){
     var numDiasSemana = fecha.getDay();
     var diasDeSemana = new Array('Domingo','Lunes','Marte','Miércoles','Jueves','Viernes','Sábado');
     var diasLetras =  diasDeSemana[fecha.getDay()]; // llamo a fecha
-
+    return diasLetras;
+ } 
+ console.log(textodia(fecha));
+    function textoMes(fecha){
     var Mes = new Array( 'Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
     var mesLetra = Mes[fecha.getMonth()];
+    return mesLetra;
+    }
+    console.log(textoMes(fecha));
 
     var diaMes = (fecha.getDate());
+    // document.getElementById('diaSemana').innerHTML = textodia(fecha);
+    // console.log(textodia(fecha));
+     document.getElementById('diaSemana').innerHTML = textodia(fecha) ;
+    let darleColor = document.getElementById('diaSemana');
+    darleColor.style.color = 'rgb(13, 23, 173)';
+    console.log(dia);
+    document.getElementById('dia').innerHTML = diaMes;
+    document.getElementById('mesLetra').innerHTML = textoMes(fecha);
+    console.log(mesLetra);
+    
 
-    var devolver = 'Hoy es un gran día, ' + diasLetras + ' ' + diaMes +' de '+ mesLetra;
-    return devolver;
-}
-console.log(textoFecha(fecha));
-document.getElementById ('llamo_fecha').innerHTML = textoFecha(fecha);
+
