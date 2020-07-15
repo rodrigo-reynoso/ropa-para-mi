38d77a496e7952c6d91b408dc6acdaefd6dce6cb
rodrigo-reynoso <rodrigosreynoso33@gmail.com>
2020-07-15 20:31:02 -0300

    enviar texto de input a otro input

---
+++ app.js
@@ -0,0 +1,21 @@
+// function enviarTexto() {
+//   let texto = document.getElementById("icono").value;
+//   texto = document.getElementById("search").value;
+// }
+console.log("hola");
+// console.log(texto);
+// let texto1 = document.getElementById("search1").value;
+// console.log(enviarTexto());
+
+// let metodo = document.getElementById("form");
+// console.log(metodo);
+let mostrarLoQueEscribo = function () {
+  let texto3 = document.getElementById("search1").value;
+  //   var texto4 = document.getElementById("search").value;
+  console.log(texto3);
+  texto4 = texto3;
+};
+console.log();
+
+var texto = document.getElementById("search").value;
+console.log(texto);
