e17f0700ad8efe6f24ca983865258b92e9975f20
rodrigo-reynoso <rodrigosreynoso33@gmail.com>
2020-07-18 19:22:35 -0300

    pongo valor de busqueda en el button de el index

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-// function enviarTexto() {
-//   let texto = document.getElementById("icono").value;
-//   texto = document.getElementById("search").value;
-// }
 
-let mostrarLoQueEscribo = function () {
-  let texto3 = document.getElementById("search1").value;
-  //   var texto4 = document.getElementById("search").value;
-  console.log(texto3);
-  texto4 = texto3;
-};
--- busqueda.js
+++ busqueda.js
@@ -1,11 +1,23 @@
 var texto = document.getElementById("search").value;
 console.log(texto);
 
-
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
-const busqueda = urlParams.get('busqueda')
+const busqueda = urlParams.get("busqueda");
 console.log(busqueda);
 
 document.getElementById("search").value = busqueda;
+
+let mostrarLoQueEscribo = function () {
+  let texto3 = document.getElementById("search").value;
+  const icono = window.location.search1;
+  console.log(icono);
+  const urlParams = new URLSearchParams(icono);
+  const busqueda1 = urlParams.get("busqueda");
+  console.log(busqueda1);
+  //   var texto4 = document.getElementById("search").value;
+  // console.log(texto3);
+  // texto4 = texto3;
+  document.getElementById("search").value = busqueda;
+};
