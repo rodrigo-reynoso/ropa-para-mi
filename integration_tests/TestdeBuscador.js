e08dfa0bca63a4e230f9fd87fa604a98f4d80486
unknown <juanpablo.isabella@gmail.com>
2020-09-24 16:45:14 -0300

    test beta

--- integration_tests/TestdeBuscador.js
+++ integration_tests/TestdeBuscador.js
@@ -1,3 +1,4 @@
+const  assert = require("assert");
 const { Builder, By, } = require("selenium-webdriver");
 
 const driver = new Builder()
@@ -6,11 +7,21 @@ const driver = new Builder()
 
 async function TestdeBuscador() {
   try {
-    await driver.get("http://127.0.0.1:5500/index.html");
+    await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
     await driver.findElement(By.name("busqueda")).sendKeys("camisa");
+    await driver.findElement(By.id("icono")).click();
+    await driver.findElement(By.id("search")).getText();
+    
+    const text = await driver.findElement(By.id("search")).getText();
+    assert.equal(text, "camisa");
+
+    
   } catch (error) {
     console.log(error);
   }
+
 }
 
 TestdeBuscador();
+
+
