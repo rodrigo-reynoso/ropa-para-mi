f33bf45a3b7623acf698a38771f224d0f61b1a4c
unknown <juanpablo.isabella@gmail.com>
2020-09-23 21:20:42 -0300

    version de test sin ultimo paso

---
+++ integration_tests/TestdeBuscador.js
@@ -0,0 +1,17 @@
+const { Builder, By, } = require("selenium-webdriver");
+
+const driver = new Builder()
+.forBrowser("firefox")
+.build();
+
+async function TestdeBuscador() {
+  try {
+    await driver.get("http://127.0.0.1:5500/index.html");
+    await driver.findElement(By.name("busqueda")).sendKeys("camisa");
+    await driver.findElement(By.id("icono")).click();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+TestdeBuscador();
