7d97e6e8a4fc9563ec64d6ffe3a196807e1f3581
unknown <juanpablo.isabella@gmail.com>
2020-09-25 22:43:53 -0300

    test

--- integration_tests/TestdeBuscador.js
+++ integration_tests/TestdeBuscador.js
@@ -1,30 +1,12 @@
-const  assert = require("assert");
 const { Builder, By, } = require("selenium-webdriver");
-
-
- 
-
 const driver = new Builder().forBrowser("firefox").build();
+const  assert = require("assert");
 
-async function TestdeBuscador() {
-  try {
+     driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
+     driver.findElement(By.name("busqueda")).sendKeys("camisa");
+     driver.findElement(By.id("icono")).click();
+     driver.findElement(By.id("search")).getText().then(function(txt){
+    console.log("el elemento es" + txt);    
+    });
     
-    
-    await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
-    await driver.findElement(By.name("busqueda")).sendKeys("camisa");
-    await driver.findElement(By.id("icono")).click();
-    await driver.findElement(By.id("search")).getText();
-  
-    const text = await driver.findElement(By.id("search")).getText();
-    
-    assert.equal(text, "camisa");
-  
-  } catch (error) {
-    console.log(error);
-  }
-
-}
-
-TestdeBuscador();
-
 
