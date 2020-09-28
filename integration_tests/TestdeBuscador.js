294c411d649468a4563bba2e3b7437a89980b95b
unknown <juanpablo.isabella@gmail.com>
2020-09-28 18:35:16 -0300

    test

--- integration_tests/TestdeBuscador.js
+++ integration_tests/TestdeBuscador.js
@@ -5,7 +5,7 @@ const  assert = require("assert");
      driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
      driver.findElement(By.name("busqueda")).sendKeys("camisa");
      driver.findElement(By.id("icono")).click();
-     driver.findElement(By.id("search")).getText().then(function(txt){
+     driver.findElement(By.id("search")).getAttribute("busqueda...").then(function(txt){
     console.log("el elemento es" + txt);    
     });
     
