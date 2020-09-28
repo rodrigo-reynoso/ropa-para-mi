d4a2099b746b7ccddc80fa4203d4a8c95595e895
unknown <juanpablo.isabella@gmail.com>
2020-09-28 19:41:56 -0300

    test de input

--- integration_tests/TestdeBuscador.js
+++ integration_tests/TestdeBuscador.js
@@ -1,12 +1,19 @@
-const { Builder, By, } = require("selenium-webdriver");
+const { Builder, By, until } = require("selenium-webdriver");
 const driver = new Builder().forBrowser("firefox").build();
-const  assert = require("assert");
 
-     driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
-     driver.findElement(By.name("busqueda")).sendKeys("camisa");
-     driver.findElement(By.id("icono")).click();
-     driver.findElement(By.id("search")).getAttribute("busqueda...").then(function(txt){
-    console.log("el elemento es" + txt);    
+async function TestdeBusqueda()
+
+     {
+
+     await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
+     await driver.findElement(By.name("busqueda")).sendKeys("camisa");
+     await driver.findElement(By.id("icono")).click(); try{
+     await driver.wait(until.titleIs("Página de Busqueda"), 1000);
+     }
+     finally {await driver.findElement(By.id("search")).getAttribute("type").then(function(txt){
+    console.log("el input es " + txt);    
     });
-    
+}
+}
 
+TestdeBusqueda();
