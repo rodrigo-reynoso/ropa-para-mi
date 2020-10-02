2456fc346dad16e71281549a5d60bb359b3e8cf5
unknown <juanpablo.isabella@gmail.com>
2020-10-02 17:26:15 -0300

    assert,formato,const

--- integration_tests/TestdeBuscador.js
+++ integration_tests/TestdeBuscador.js
@@ -3,6 +3,7 @@ const driver = new Builder().forBrowser("firefox").build();
 
 async function TestdeBusqueda()
 
+<<<<<<< Updated upstream
      {
 
      await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
@@ -14,6 +15,27 @@ async function TestdeBusqueda()
     console.log("el input es " + txt);    
     });
 }
+=======
+async function TestdeBusqueda() {
+
+    const RopaBuscada = "pantalon";
+
+
+
+    await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
+    await driver.findElement(By.name("busqueda")).sendKeys(RopaBuscada);
+    await driver.findElement(By.id("icono")).click(); try {
+        await driver.wait(until.titleIs("Página de Busqueda"), 1000);
+    }                                                  /*en .getAttribute podes poner cualquier atributo del "input" de busqueda.html en este caso es "type"  */
+    finally {
+        await driver.findElement(By.css("input")).getAttribute("value").then(function (SearchValue) {
+            assert.strictEqual(RopaBuscada, SearchValue, "el test fallo");
+
+
+        });
+    }
+
+>>>>>>> Stashed changes
 }
 
 TestdeBusqueda();
