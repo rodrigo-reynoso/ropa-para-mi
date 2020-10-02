d681b85dcaee646e17677b3301ea8a9359bde0ad
unknown <juanpablo.isabella@gmail.com>
2020-10-02 17:30:25 -0300

    assert,formato, const

--- integration_tests/TestdeBuscador.js
+++ integration_tests/TestdeBuscador.js
@@ -1,21 +1,7 @@
 const { Builder, By, until } = require("selenium-webdriver");
 const driver = new Builder().forBrowser("firefox").build();
+const assert = require("assert");
 
-async function TestdeBusqueda()
-
-<<<<<<< Updated upstream
-     {
-
-     await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
-     await driver.findElement(By.name("busqueda")).sendKeys("camisa");
-     await driver.findElement(By.id("icono")).click(); try{
-     await driver.wait(until.titleIs("Página de Busqueda"), 1000);
-     }                                                  /*en .getAttribute podes poner cualquier atributo del "input" de busqueda.html en este caso es "type"  */
-     finally {await driver.findElement(By.tagName("input")).getAttribute("value").then(function(txt){
-    console.log("el input es " + txt);    
-    });
-}
-=======
 async function TestdeBusqueda() {
 
     const RopaBuscada = "pantalon";
@@ -35,7 +21,7 @@ async function TestdeBusqueda() {
         });
     }
 
->>>>>>> Stashed changes
+
 }
 
 TestdeBusqueda();
