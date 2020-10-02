90bb6f487c09737b1dbe13d8e185b2639080c87c
unknown <juanpablo.isabella@gmail.com>
2020-10-02 16:40:49 -0300

    format, new assert, new const

--- integration_tests/TestdeBuscador.js
+++ integration_tests/TestdeBuscador.js
@@ -1,10 +1,10 @@
-const { Builder, By, until } = require("selenium-webdriver");
+const { Builder, By, until, } = require("selenium-webdriver");
 const driver = new Builder().forBrowser("firefox").build();
+const assert = require("assert");
 
-async function TestdeBusqueda()
 
-     {
 
+<<<<<<< Updated upstream
      await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
      await driver.findElement(By.name("busqueda")).sendKeys("camisa");
      await driver.findElement(By.id("icono")).click(); try{
@@ -14,6 +14,28 @@ async function TestdeBusqueda()
     console.log("el input es " + txt);    
     });
 }
+=======
+
+
+async function TestdeBusqueda() {
+
+    const RopaBuscada = "pantalon";
+
+
+
+    await driver.get("http://127.0.0.1:5500/index.html"); /*este link tiene que ser modificado dependiendo del repositorio local de cada uno*/
+    await driver.findElement(By.name("busqueda")).sendKeys(RopaBuscada);
+    await driver.findElement(By.id("icono")).click(); try {
+        await driver.wait(until.titleIs("Página de Busqueda"), 1000);
+    }                                                  /*en .getAttribute podes poner cualquier atributo del "input" de busqueda.html en este caso es "type"  */
+    finally {
+        await driver.findElement(By.css("input")).getAttribute("value").then(function (SearchValue) {
+            assert.strictEqual(RopaBuscada, SearchValue, "el test fallo");
+
+
+        });
+    }
+>>>>>>> Stashed changes
 }
 
 TestdeBusqueda();
